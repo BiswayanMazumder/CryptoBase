37e34b7209169929b685264484f0d91c2d4c4b6e
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-24 13:54:09 +0530

    Login Page Started Creating

---
+++ lib/Login Page/emaillogin.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class EmailLogin extends StatefulWidget {
+  const EmailLogin({Key? key}) : super(key: key);
+
+  @override
+  State<EmailLogin> createState() => _EmailLoginState();
+}
+
+class _EmailLoginState extends State<EmailLogin> {
+  bool islogin=true;
+  void changepage(){
+    setState(() {
+      islogin=!islogin;
+      print(islogin);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF232f3f),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF232f3f),
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child:const Icon(Icons.arrow_back,color: Colors.white,),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text('Continue Using Email ID',style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 25
+              ),),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                height: 40,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  border: Border.all(color:const Color(0xFF232f6f))
+                ),
+                child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: (){
+                        changepage();
+                      },
+                      child: Text('Login',style: GoogleFonts.poppins(
+                          color: islogin?Colors.white:Colors.blueAccent,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15
+                      ),),
+                    ),
+                    InkWell(
+                      onTap: (){
+                        changepage();
+                      },
+                      child: Text('Sign Up',style: GoogleFonts.poppins(
+                          color: islogin?Colors.blueAccent:Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15
+                      ),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueGrey,width: 0.9)
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: '  Email',
+                    hintStyle: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                    )
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueGrey,width: 0.9)
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: '  Password',
+                      hintStyle: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      )
+                  ),
+                ),
+              ),
+            ),
+            islogin?Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Forgot Password',style: GoogleFonts.poppins(color: Colors.blueGrey,
+                    fontWeight: FontWeight.w400
+                  ),),
+                ),
+              ],
+            ):const Text(''),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Login Page/getstarted.dart
+++ lib/Login Page/getstarted.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cryptobase/Login%20Page/emaillogin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 class GetStarted extends StatefulWidget {
   const GetStarted({Key? key}) : super(key: key);
 
@@ -31,7 +34,47 @@ class _GetStartedState extends State<GetStarted> {
   int currentIndex = 0;
   late PageController _pageController;
   late Timer _timer;
-
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+  FirebaseFirestore _firestore=FirebaseFirestore.instance;
+  bool isloading=false;
+  void _handleSignIn() async {
+    try {
+      setState(() {
+        isloading=true;
+      });
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+        print('Username ${_googleSignIn.currentUser!.displayName}');
+        print('Photo ${_googleSignIn.currentUser!.photoUrl}');
+        // await _googleSignIn.signOut();
+        final User? user = authResult.user;
+        if (user != null) {
+          print('User UID: ${user.uid}');
+          await _firestore.collection('User Details').doc(user.uid).set({
+            'Name':_googleSignIn.currentUser!.displayName,
+            'Email':_googleSignIn.currentUser!.email,
+            'Profile Pic':_googleSignIn.currentUser!.photoUrl!=null?_googleSignIn.currentUser!.photoUrl:'https://plus.unsplash.com/premium_photo-1700268374954-f06052915608?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'Time Of Registration':FieldValue.serverTimestamp(),
+          });
+          setState(() {
+            isloading=false;
+          });
+          // Now the user should appear in Firebase Authentication
+        }
+        // Now the user should appear in Firebase Authentication
+      } else {
+        // User cancelled sign-in
+      }
+    } catch (error) {
+      print('Error signing in: $error');
+    }
+  }
   @override
   void initState() {
     super.initState();
@@ -174,7 +217,9 @@ class _GetStartedState extends State<GetStarted> {
               Column(
                 children: [
                   Container(
-                    child: ElevatedButton(onPressed: (){},
+                    child: ElevatedButton(onPressed: (){
+                      _handleSignIn();
+                    },
                         style: const ButtonStyle(
                             elevation: MaterialStatePropertyAll(5),
                             shadowColor: MaterialStatePropertyAll(Colors.black),
@@ -208,7 +253,9 @@ class _GetStartedState extends State<GetStarted> {
                       border: Border.all(color: Colors.white,width:0.5,),
                       borderRadius: BorderRadius.circular(50)
                     ),
-                    child: ElevatedButton(onPressed: (){},
+                    child: ElevatedButton(onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => EmailLogin(),));
+                    },
                         style: const ButtonStyle(
                             elevation: MaterialStatePropertyAll(0),
                             backgroundColor: MaterialStatePropertyAll(Colors.transparent)
