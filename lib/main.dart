bbe2785d3b03fe6053675983e5433cfe2a4a275b
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-24 12:43:55 +0530

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:cryptobase/Login%20Page/getstarted.dart';
+import 'package:cryptobase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
