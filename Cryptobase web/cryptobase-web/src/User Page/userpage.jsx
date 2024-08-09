62daea240c388174b2c862bd779ccf904a0014d4
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-09 14:35:20 +0530

    Firestore integrated

--- Cryptobase web/cryptobase-web/src/User Page/userpage.jsx
+++ Cryptobase web/cryptobase-web/src/User Page/userpage.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect } from 'react'
 import Sidebar from '../Side Bar/sidebar'
+import { getFirestore,getDocs, collection } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCxkw9hq-LpWwGwZQ0APU0ifJ5JQU2T8Vk",
+  authDomain: "cryptobase-admin.firebaseapp.com",
+  projectId: "cryptobase-admin",
+  storageBucket: "cryptobase-admin.appspot.com",
+  messagingSenderId: "1090236390686",
+  appId: "1:1090236390686:web:856b22fd209b267b89fd0f",
+  measurementId: "G-LTBWYEEF6E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 export default function Userpage() {
     useEffect(() => {
         document.title = 'CryproBase Admin Panel';
       }, []);
   return (
-    
     <div className="pages">
         <Sidebar />
         <div className="detailspage">
