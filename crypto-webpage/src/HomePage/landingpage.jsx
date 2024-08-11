b18cfb40698b69aa9bb34443decf208797a2a6eb
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-11 10:23:29 +0530

    Home Page creation responsiveness not yet added

--- crypto-webpage/src/HomePage/landingpage.jsx
+++ crypto-webpage/src/HomePage/landingpage.jsx
@@ -5,7 +5,7 @@ export default function Landingpage() {
     function setdoctitle() {
         document.title = 'Buy Bitcoin, Cryptocurrency at India’s Largest Exchange | Trading Platform | CryptoBase'
     }
-    
+
     return (
         <>
             <div className="webbody" onLoad={setdoctitle()}>
@@ -14,20 +14,37 @@ export default function Landingpage() {
                     <div className="firstpartcontents">
                         <div className="texts">
                             <div className="headers">
-                            <Typewriter
+                                <Typewriter
                                     options={{
-                                        strings: ['India का  Ripple ','India का  Solana ','India का  Tron ',
-                                        'India का  Matic ','India का  Etherium ','India का  Shiba Inu '],
+                                        strings: ['India का  Ripple ', 'India का  Solana ', 'India का  Tron ',
+                                            'India का  Matic ', 'India का  Etherium ', 'India का  Shiba Inu '],
                                         autoStart: true,
                                         loop: true,
                                         cursor: '|',
                                         delay: 75,
                                     }}
-                                /> 
+                                />
                                 Exchange
                                 <div className="subtext">
                                     Trusted By 15 million+ Indians
+
                                 </div>
+                                <div className="downloadbtn">
+                                    <div className="externalbtns">
+                                        <div className="playstorebtn">
+                                            <img src="https://media.wazirx.com/web_assets/download_from_playstore_btn/dark/1x.svg" alt="" className='playstoreimg' />
+                                        </div>
+                                        <div className="playstorebtn">
+                                            <img src="https://media.wazirx.com/web_assets/download_from_appstore_btn/dark/1x.svg" alt="" className='playstoreimg' />
+                                        </div>
+                                    </div>
+                                    <Link className="signupbtn">
+                                    <div >
+                                    SIGN UP
+                                    </div>
+                                    </Link>
+                                </div>
+
                             </div>
                             <img src="https://media.wazirx.com/web_assets/landing_page_hero_app_screen/dark/2x.png" alt=""
                                 loading='lazy' className='heroimage' />
